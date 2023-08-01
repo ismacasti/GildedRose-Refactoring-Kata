6214f1d26db0cec9f787347ce608248f16e4e671
Ismael Castiñeira <isma.casti@gmail.com>
2023-08-01 18:12:57 +0200

    test: light testing framework, conjured test
    Introducing a new testing framework, that makes easy and clear
    to observe changing behaviour.
    Two new tests are implemented, a basics with the already
    existing features, and a new one for the yet unimplemented
    conjured items.

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -1,19 +1,81 @@
 package gildedrose_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
-func Test_Foo(t *testing.T) {
-	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
-	}
+type itemBeforeAfter struct {
+	Name                        string
+	BeforeSellIn, AfterSellIn   int
+	BeforeQuality, AfterQuality int
+}
 
+func failTest[E any, G any](what string, item itemBeforeAfter, expected E, got G) error {
+	return fmt.Errorf(
+		"On item <<%v>>, %s does not match, expected %v but got %v",
+		item,
+		what,
+		expected,
+		got,
+	)
+}
+
+func testUpdateQuality(testValues *[]itemBeforeAfter) error {
+	var items = make([]*gildedrose.Item, len(*testValues))
+
+	for i, testValue := range *testValues {
+		items[i] = &gildedrose.Item{
+			Name:    testValue.Name,
+			Quality: testValue.BeforeQuality,
+			SellIn:  testValue.BeforeSellIn,
+		}
+	}
 	gildedrose.UpdateQuality(items)
 
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for i, testValue := range *testValues {
+		if items[i].Name != testValue.Name {
+			return failTest("Name", testValue, testValue.Name, items[i].Name)
+		}
+		if items[i].Quality != testValue.AfterQuality {
+			return failTest("Quality", testValue, testValue.AfterQuality, items[i].Quality)
+		}
+		if items[i].SellIn != testValue.AfterSellIn {
+			return failTest("SellIn", testValue, testValue.AfterSellIn, items[i].SellIn)
+		}
+	}
+	return nil
+}
+
+func TestBasic(t *testing.T) {
+	var basics = []itemBeforeAfter{
+		{"+5 Dexterity Vest", 10, 9, 20, 19},
+		{"Aged Brie", 2, 1, 0, 1},
+		{"Elixir of the Mongoose", 5, 4, 7, 6},
+		{"Sulfuras, Hand of Ragnaros", 0, 0, 80, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, -1, 80, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 14, 20, 21},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 9, 49, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 4, 49, 50},
+	}
+
+	err := testUpdateQuality(&basics)
+	if err != nil {
+		t.Fatalf("Test failed: %v", err)
+	}
+
+}
+
+func TestConjured(t *testing.T) {
+	var conjuredItems = []itemBeforeAfter{
+		{"Conjured Mana Cake", 3, 2, 6, 4},          // normal conjured
+		{"Conjured Lottery ticket", 50, 49, 50, 48}, // conjured already at 50 quality
+	}
+
+	err := testUpdateQuality(&conjuredItems)
+	if err != nil {
+		t.Fatalf("Test failed: %v", err)
 	}
 }
